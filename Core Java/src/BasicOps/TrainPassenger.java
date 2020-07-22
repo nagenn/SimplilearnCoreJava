599814fe8351c0fcf51b0c1bf5940f8ef58cc136
nagenn <nvmurti@netscape.net>
2020-07-22 23:11:58 +0530

    Added Constructors example

---
+++ Core Java/src/BasicOps/TrainPassenger.java
@@ -0,0 +1,30 @@
+package BasicOps;
+
+import java.util.Random;
+
+public class TrainPassenger {
+	private long pnr;
+	private String name;
+	private int age;
+	private char gender;
+	
+	//constructor - ok to leave it with default access
+	TrainPassenger(String varnme, int varage, char vargen) {
+		pnr =  new Random().nextLong();
+		name = varnme;
+		age = varage;
+		gender  = vargen;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public char getGen() {
+		return gender;
+	}
+	public long getPnr() {
+		return pnr;
+	}
+}
